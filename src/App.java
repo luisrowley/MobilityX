8778afbc2f0e4d2f448ca8ac0a28654b050d7707
luisrowley <louisdebenito@gmail.com>
2025-03-14 09:52:15 +0100

    added initial boilerplate

--- src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        String x = "asd";
-        System.out.println("Hello, World!");
-    }
-}
---
+++ src/main/java/com/mobilityX/models/Usuario.java
@@ -0,0 +1,5 @@
+package main.java.com.mobilityX.models;
+
+public class Usuario {
+    
+}
